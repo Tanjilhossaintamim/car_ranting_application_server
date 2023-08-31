dc913b38705a3c11fd1be3d11b0ae02701f24d53
Tanjil Hossain <tanjilhossain845.com>
2023-08-31 12:29:27 +0600

    add allowhost

--- car/settings.py
+++ car/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-lh9ak!y%8u8^r9t=j+(jm$r9f^bc+!ewnvjh*1ob$m=9!@q19w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carranting.onrender.com']
 
 
 # Application definition
