aa05e004437e732f0f934621924171a98ff417c8
Tanjil Hossain <tanjilhossain845.com>
2023-08-31 12:08:41 +0600

    add static file directory in settings

--- car/settings.py
+++ car/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = STATIC_DIR
 MEDIA_URL = 'media/'
 MEDIA_ROOT = MEDIA_DIR
 
