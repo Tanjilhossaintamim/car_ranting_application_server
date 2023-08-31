e8c8ebdb7f815ba69b9e0af7727c32d3cb097dea
Tanjil Hossain <tanjilhossain845.com>
2023-08-31 18:55:14 +0600

    add a cors origin

--- car/settings.py
+++ car/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "https://car-ranting-application-client.vercel.app",
-    "https://localhost:5173"
+    "http://localhost:5173"
 
 ]
 
